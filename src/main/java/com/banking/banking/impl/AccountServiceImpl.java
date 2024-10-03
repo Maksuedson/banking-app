a5f8b6edeb81b2ca90deead16d847f67b29ef00d
Maksuedson <maksuedson@hotmail.com>
2024-10-03 16:13:25 -0300

    AccountServiceImpl class created

---
+++ src/main/java/com/banking/banking/impl/AccountServiceImpl.java
@@ -0,0 +1,28 @@
+package com.banking.banking.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.banking.banking.dto.AccountDto;
+import com.banking.banking.entity.Account;
+import com.banking.banking.mapper.AccountMapper;
+import com.banking.banking.repository.AccountRepository;
+import com.banking.banking.service.AccountService;
+
+@Service
+public class AccountServiceImpl implements AccountService{
+	
+	private AccountRepository repo;
+
+	public AccountServiceImpl(AccountRepository repo) {
+		this.repo = repo;
+	}
+
+
+	@Override
+	public AccountDto createAccount(AccountDto accountDto) {
+		Account account = AccountMapper.mapToAccount(accountDto);
+		Account saveAccount = repo.save(account);
+		return AccountMapper.mapToAccountDto(saveAccount);
+	}
+
+}
